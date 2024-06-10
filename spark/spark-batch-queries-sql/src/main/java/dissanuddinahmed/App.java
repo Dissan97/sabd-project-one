67037d17c6927758b31cfe9d7d9e3667e1e0c30a
dix <pc.dissan@gmail.com>
2024-06-10 17:36:21 +0200

    [SABD2324 1.3]

--- spark/spark-batch-queries-sql/src/main/java/dissanuddinahmed/App.java
+++ spark/spark-batch-queries-sql/src/main/java/dissanuddinahmed/App.java
@@ -18,27 +18,27 @@ public class App
     public static void firtsQuerySql(SparkSession spark) {
     
       Dataset<Row> resDf = spark.sql(
-        "SELECT date, vault_id, number_of_failures " +
-        "FROM (SELECT date, vault_id, COUNT(failure) as number_of_failures "+
+        "SELECT date as date_of_failure, vault_id, failures_count " +
+        "FROM (SELECT date, vault_id, COUNT(failure) as failures_count "+
         "FROM diskFailures "+
         "WHERE failure = '1' "+
         "GROUP BY date, vault_id "+
         "HAVING COUNT(failure) BETWEEN 2 AND 4) "+
-        "ORDER BY date"
+        "ORDER BY failures_count"
       );
       //resDf.printSchema();
       //res.show();
       resDf.write().mode("overwrite")
-				.option("header", "true")
-				.option("sep", ",")
-				.csv("hdfs://namenode:54310/user/spark/pipeline/results/firtsQuery");
+		.option("header", "true")
+		.option("sep", ",")
+		.csv("hdfs://namenode:54310/user/spark/pipeline/results/firtsQuery");
     }
 
 
     public static void secondQuerySql(SparkSession spark) {
     
       Dataset<Row> resModelDf = spark.sql(
-        "SELECT model, COUNT(failure) as number_of_failures "+
+        "SELECT model, COUNT(failure) as failures_count "+
         "FROM diskFailures "+
         "WHERE failure = '1' "+
         "GROUP BY model "+
@@ -52,32 +52,32 @@ public class App
                 .csv("hdfs://namenode:54310/user/spark/pipeline/results/secondQuery/TopModels");
       Dataset<Row> resultVaultPerModelDf = spark.sql(
         "WITH perVaultFailure AS ( " +
-        "  SELECT vault_id, COUNT(*) AS failure_count " +
+        "  SELECT vault_id, COUNT(*) AS failures_count " +
         "  FROM diskFailures " +
         "  WHERE failure = 1 " +
         "  GROUP BY vault_id " +
         "), " +
         "topPerFailureVault AS ( " +
-        "  SELECT vault_id, failure_count " +
+        "  SELECT vault_id, failures_count " +
         "  FROM perVaultFailure " +
-        "  ORDER BY failure_count DESC " +
+        "  ORDER BY failures_count DESC" +
         "  LIMIT 10 " +
         "), " +
         "topPerVaultWithFailureDf AS ( " +
-        "  SELECT tpf.vault_id, df.model, tpf.failure_count " +
+        "  SELECT tpf.vault_id, df.model, tpf.failures_count " +
         "  FROM topPerFailureVault tpf " +
         "  JOIN diskFailures df " +
         "  ON tpf.vault_id = df.vault_id " +
         "  WHERE df.failure = 1 " +
         "), " +
         "vaultModelDf AS ( " +
-        "  SELECT vault_id, failure_count, collect_set(model) AS models " +
+        "  SELECT vault_id, failures_count, collect_set(model) AS list_of_models " +
         "  FROM topPerVaultWithFailureDf " +
-        "  GROUP BY vault_id, failure_count " +
-        "  ORDER BY failure_count DESC " +
+        "  GROUP BY vault_id, failures_count " +
+        "  ORDER BY failures_count DESC " +
         "  LIMIT 10 " +
         ") " +
-        "SELECT vault_id, failure_count, concat_ws(';', models) AS models " +
+        "SELECT vault_id, failures_count, concat_ws(';', models) AS models " +
         "FROM vaultModelDf"
 
     );
@@ -93,15 +93,15 @@ public class App
     public static void main( String[] args ){
       
       try (SparkSession spark = SparkSession.builder()
-				.appName(APP_NAME)
-				.master("spark://spark:7077")
-				.getOrCreate()) {
+			.appName(APP_NAME)
+			.master("spark://spark:7077")
+			.getOrCreate()) {
 
-			  Dataset<Row> diskFailureDf = spark.read().option("header", true)
-					  .parquet("hdfs://namenode:54310/user/spark/pipeline/preprocessed/data.parquet");
+			Dataset<Row> diskFailureDf = spark.read().option("header", true)
+				  .parquet("hdfs://namenode:54310/user/spark/pipeline/preprocessed/data.parquet");
 
-			  LOGGER.info(LOG_NOTICE+"DISK FAILURE SCHEMA"+LOG_NOTICE);
-			  diskFailureDf.printSchema();
+			LOGGER.info(LOG_NOTICE+"DISK FAILURE SCHEMA"+LOG_NOTICE);
+			diskFailureDf.printSchema();
         diskFailureDf.createOrReplaceTempView("tempView");
         
         Dataset<Row> diskFailureAdjusted = spark.sql(
